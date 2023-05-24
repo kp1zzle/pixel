adb298c0c0b8f33f5a0d07727cb48d0a1f42ab08
Kieran Sagar Parikh <kieran@parikh.ca>
2023-05-24 19:18:43 -0400

    use image as buffer

--- src/index.ts
+++ src/index.ts
@@ -2,49 +2,63 @@ import * as P5 from 'p5';
 import {mountainEffect} from "./mountainEffect";
 
 let sketch = (s: P5) => {
-    const COLUMNS_AND_ROWS = 100;
-    let PIXEL_WIDTH = 10;
+    const COLUMNS_AND_ROWS = 256;
+    const CANVAS_SIZE = 1000
     let t = 0;
     let globalTimeMultiplier = 0.5
     let effects = [
-        new mountainEffect(0, 0, 40, 30, "#0c0eef", null, 0.5,true),
-        new mountainEffect(0, 0, 10, 100, "#000000", null, -0.8),
-        new mountainEffect(0, 40, 20, 100, "#000000", null, 1.7),
-        new mountainEffect(0, 0, 150, 100, "#FFFFFF", "#000000"),
+        new mountainEffect(0, 0, 40, 30, s.color("#0c0eef"), null, 0.5,true),
+        new mountainEffect(0, 0, 10, 256, s.color("#000000"), null, -0.8),
+        new mountainEffect(0, 40, 100, 256, s.color("#000000"), null, 3),
+        new mountainEffect(0, 0, 350, 256, s.color("#FFFFFF"), s.color("#000000")),
     ]
+    let frameRateVisible = true;
 
     s.setup = () => {
-
-        PIXEL_WIDTH = s.min( window.innerWidth/COLUMNS_AND_ROWS, window.innerHeight/COLUMNS_AND_ROWS)
-        s.createCanvas(PIXEL_WIDTH*COLUMNS_AND_ROWS, PIXEL_WIDTH*COLUMNS_AND_ROWS)
+        s.createCanvas(CANVAS_SIZE, CANVAS_SIZE)
         s.background(220);
+        s.pixelDensity(1)
+        s.noSmooth()
     }
 
     s.draw = () => {
         s.noStroke()
+        s.background(255)
 
         for (let effect of effects) {
             effect.calculateFrameForTime(t, s.noise)
         }
 
+        let img = s.createImage(COLUMNS_AND_ROWS, COLUMNS_AND_ROWS)
         for (let column = 0; column < COLUMNS_AND_ROWS; column++) {
             for (let row = 0; row < COLUMNS_AND_ROWS; row++) {
                 for (let effect of effects) {
                     let color = effect.pixelColor(column, row)
                     if (color != null) {
-                        s.fill(color)
+                        img.set(column, row, color)
                         break;
                     }
-                    s.fill("#ffffff")
                 }
-                s.rect(column*PIXEL_WIDTH, row*PIXEL_WIDTH, PIXEL_WIDTH, PIXEL_WIDTH)
             }
         }
+        img.updatePixels()
+        s.image(img, 0, 0, CANVAS_SIZE, CANVAS_SIZE)
+
         if (s.frameRate() !== 0) {
             t += 1/s.frameRate() * globalTimeMultiplier
+            if (frameRateVisible) {
+                s.fill(0)
+                s.text(s.frameRate().toFixed(0), 10, 10)
+            }
         }
-
     }
+
+    s.keyPressed = () => {
+        if (s.key === ' ') {
+            frameRateVisible = !frameRateVisible
+        }
+    }
+
 
 }
 
