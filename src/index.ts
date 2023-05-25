56f3684fadc8d378815c1a412416c47baa1fc298
Kieran Parikh <kieran@parikh.ca>
2023-05-24 21:43:23 -0400

    stacked mountains

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
 import * as P5 from 'p5';
 import {mountainEffect} from "./mountainEffect";
 import {bouncingRect} from "./bouncingRect";
+import {areaEffect} from "./areaEffect";
 
 let sketch = (s: P5) => {
     const COLUMNS_AND_ROWS = 256;
-    const CANVAS_SIZE = 1000
+    const CANVAS_SIZE = 600
     let t = 0;
     let globalTimeMultiplier = 0.5
-    let effects = [
-        new mountainEffect(0, 0, 40, 30, s.color("#0c0eef"), null, 0.5,true),
-        new bouncingRect(5, 10, 0, 256, 40, 256, s.color("#0c0eef"), true, false),
-        new bouncingRect(5, 10, 0, 256, 45, 256, s.color("#0c0eef"), true, false, 2),
-        new bouncingRect(5, 10, 0, 256, 50, 256, s.color("#0c0eef"), true, false, 5),
-        new bouncingRect(10, 5, 0, 256, 50, 256, s.color("#0c0eef"), false, true, 1.1),
-        new mountainEffect(0, 0, 10, 256, s.color("#000000"), null, -0.8),
-        new mountainEffect(0, 40, 100, 256, s.color("#000000"), null, 3),
-        new mountainEffect(0, 0, 350, 256, s.color("#FFFFFF"), s.color("#000000")),
+    // let effects = [
+    //     new mountainEffect(0, 0, 40, 30, s.color("#0c0eef"), null, 0.5,true),
+    //     new bouncingRect(5, 10, 0, 256, 40, 256, s.color("#0c0eef"), true, false),
+    //     new bouncingRect(5, 10, 0, 256, 45, 256, s.color("#0c0eef"), true, false, 2),
+    //     new bouncingRect(5, 10, 0, 256, 50, 256, s.color("#0c0eef"), true, false, 5),
+    //     new bouncingRect(10, 5, 0, 256, 50, 256, s.color("#0c0eef"), false, true, 1.1),
+    //     new mountainEffect(0, 0, 10, 256, s.color("#000000"), null, -0.8),
+    //     new mountainEffect(0, 40, 100, 256, s.color("#000000"), null, 3),
+    //     new mountainEffect(0, 0, 350, 256, s.color("#FFFFFF"), s.color("#000000")),
+    // ]
+    let effects: areaEffect[] = [
+        new mountainEffect(0, 0, 50, 256, s.color("#000000"), null, -0.8),
     ]
     let frameRateVisible = true;
 
@@ -24,6 +28,23 @@ let sketch = (s: P5) => {
         s.background(220);
         s.pixelDensity(1)
         s.noSmooth()
+
+        let colors = [
+            s.color("#0c0eef"),
+            s.color("#747474"),
+            s.color("#000000"),
+        ]
+
+        for (let i = 0; i < 5; i++) {
+            let u = 1
+            if (i%2 ===0) {
+                u = -1
+            }
+            effects.push(
+                new mountainEffect(0, 40*i, 256-(40*i), 256, null, s.random(colors), u*(0.5 + .4*i),),
+            )
+        }
+        effects = effects.reverse()
     }
 
     s.draw = () => {
@@ -61,6 +82,13 @@ let sketch = (s: P5) => {
     s.keyPressed = () => {
         if (s.key === ' ') {
             frameRateVisible = !frameRateVisible
+        }
+        if (s.keyCode === s.ENTER) {
+            if (s.isLooping()) {
+                s.noLoop()
+            } else {
+                s.loop()
+            }
         }
     }
 
